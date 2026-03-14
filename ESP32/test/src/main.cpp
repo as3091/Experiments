9f8c74f76c6a1dfa6959a11778eed68d7cc5ea7f
as3091 <as3091@gmail.com>
2026-03-14 12:41:52 +0530

    why is button pressed

--- ESP32/test/src/main.cpp
+++ ESP32/test/src/main.cpp
@@ -2,45 +2,36 @@
 
 const int ledPin = LED_BUILTIN;  // GPIO2 for LED
 unsigned long previousMillis = 0;  // Tracks last toggle time
-bool ledState = false;              // LED state tracker
+bool ledState = digitalRead(ledPin); // LED state tracker
+
+const int ButtonPin = 13;      // Change to GPIO13 for ON button
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.println("Starting millis blink test...");
+  pinMode(ButtonPin, INPUT_PULLUP);   // Internal pull-up
+  Serial.println("Starting button test...");
 }
 
-void check_led_state_blink(int ledPin, unsigned long &previousMillis) {
+void button_check_led_state_blink(int ledPin, unsigned long &previousMillis) {
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 1000) {  // Check every 1 second
-    previousMillis = currentMillis;
-    bool currentState = digitalRead(ledPin);   // read actual pin state from chip
-    digitalWrite(ledPin, !currentState);        // toggle it
-    Serial.println(!currentState ? "LED ON" : "LED OFF");
+  if (currentMillis - previousMillis >= 1000)  {  // Check every 1 second
+      previousMillis = currentMillis;
+      Serial.print("Current button state: ");
+      Serial.println(digitalRead(ButtonPin) == LOW ? "PRESSED" : "RELEASED");
+      if (digitalRead(ButtonPin) == LOW)
+      {
+        bool currentState = digitalRead(ledPin);   // read actual pin state from chip
+        digitalWrite(ledPin, !currentState);        // toggle it
+        Serial.println(!currentState ? "LED ON" : "LED OFF");
+      }
   }
 }
 
-
 void loop() {
-  check_led_state_blink(LED_BUILTIN, previousMillis);
+  button_check_led_state_blink(LED_BUILTIN, previousMillis);
   // led_blink(LED_BUILTIN, ledState, previousMillis);
   // Update previousMillis and ledState after the function call
   // previousMillis = millis(); // Update to current time for next check
   // ledState = !ledState; // Toggle state for next call}
 }
-
-void led_blink(int ledPin, bool &ledState, unsigned long &previousMillis) {
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 1000) {  // Check every 1 second
-    previousMillis = currentMillis;
-    ledState = !ledState;  // Toggle state
-    if (ledState) {
-      digitalWrite(ledPin, HIGH);  // ON (active-high for external LED)
-      Serial.println("LED ON");
-    } else {
-      digitalWrite(ledPin, LOW);   // OFF
-      Serial.println("LED OFF");
-    }
-  }
-}
-
