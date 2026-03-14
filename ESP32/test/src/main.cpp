deaa3438479ca17195076ef4d22c278b162c2717
as3091 <as3091@gmail.com>
2026-03-14 13:23:15 +0530

    cehk 500 ms

--- ESP32/test/src/main.cpp
+++ ESP32/test/src/main.cpp
@@ -2,11 +2,10 @@
 
 const int ledPin = LED_BUILTIN;  // GPIO2 for LED
 unsigned long previousMillis = 0;  // Tracks last toggle time
-// bool ledState = digitalRead(ledPin); // LED state tracker
+unsigned long lastInputMillis = 0;
+// int lastButtonState = HIGH;   // INPUT_PULLUP idle state
 
 const int ButtonPin = 27;      // Change to GPIO13 for button
-
-unsigned long startMillis = 0;          // set in setup()
 const unsigned long SLEEP_AFTER = 60000; // 1 minute in ms
 
 void setup() {
@@ -14,31 +13,38 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(ButtonPin, INPUT_PULLUP);   // Internal pull-up
   Serial.println("Starting button test...");
-  startMillis = millis(); // Initialize start time
+  lastInputMillis = millis(); // Initialize start time
 }
 
-void button_check_led_state_blink(int ledPin, unsigned long &previousMillis) {
+void button_check_led_state_blink(int ledPin, unsigned long &previousMillis)
+{
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= 1000)  {  // Check every 1 second
+  if (currentMillis - previousMillis >= 500)
+    {  // Check every 0.5 second
       previousMillis = currentMillis;
+      int buttonState = digitalRead(ButtonPin);
+
       Serial.print("Current button state: ");
-      Serial.println(digitalRead(ButtonPin) == LOW ? "PRESSED" : "RELEASED");
-      if (digitalRead(ButtonPin) == LOW)
-      {
-        bool currentState = digitalRead(ledPin);   // read actual pin state from chip
-        digitalWrite(ledPin, !currentState);        // toggle it
-        Serial.println(!currentState ? "LED ON" : "LED OFF");
-      }
+      Serial.println(buttonState == LOW ? "PRESSED" : "RELEASED");
+      if (buttonState == LOW) {
+        lastInputMillis = currentMillis;  // reset inactivity timer
+        {
+          bool currentState = digitalRead(ledPin);   // read actual pin state from chip
+          digitalWrite(ledPin, !currentState);        // toggle it
+          Serial.println(!currentState ? "LED ON" : "LED OFF");
+        }
+    }
   }
 }
 
 void loop() {
-  if (millis() - startMillis >= SLEEP_AFTER) {
+  button_check_led_state_blink(LED_BUILTIN, previousMillis);
+  if (millis() - lastInputMillis >= SLEEP_AFTER) {
     Serial.println("1 minute elapsed — entering deep sleep.");
     Serial.flush();
     digitalWrite(LED_BUILTIN, LOW);      // turn LED off before sleep
     esp_deep_sleep_start();              // wake requires reset or touch/ext wakeup
   }
 
-  button_check_led_state_blink(LED_BUILTIN, previousMillis);
+  
 }
