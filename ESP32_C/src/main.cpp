9505e82e027728da782e9d4425040d6d6ccca40b
as3091 <as3091@gmail.com>
2026-03-28 21:01:44 +0530

    sensor 0

--- ESP32_C/src/main.cpp
+++ ESP32_C/src/main.cpp
@@ -1,24 +1,31 @@
 #include <Arduino.h>
 
-const int relayPin = 26;  // GPIO 26
+const int SENSOR_PIN = 36;  // GPIO 26
 
 void setup() {
+
   Serial.begin(115200);
-  pinMode(relayPin, OUTPUT);
+  analogSetAttenuation(ADC_11db);  // Important for ESP32 to read up to ~3.3V properly
+  // Serial.begin(115200);
+  // pinMode(relayPin, OUTPUT);
   
-  // Start with relay OFF (HIGH for most active-low relay modules)
-  digitalWrite(relayPin, HIGH);
-  Serial.println("Relay initialized - OFF");
+  // // Start with relay OFF (HIGH for most active-low relay modules)
+  // digitalWrite(relayPin, HIGH);
+  // Serial.println("Relay initialized - OFF");
 }
 
 void loop() {
-  // Turn relay ON for 5 seconds
-  digitalWrite(relayPin, LOW);   // Activates most 5V relay modules
-  Serial.println("Relay ON");
-  delay(5000);
+  // // Turn relay ON for 5 seconds
+  // digitalWrite(relayPin, LOW);   // Activates most 5V relay modules
+  // Serial.println("Relay ON");
+  // delay(5000);
   
-  // Turn relay OFF
-  digitalWrite(relayPin, HIGH);
-  Serial.println("Relay OFF");
-  delay(5000);
+  // // Turn relay OFF
+  // digitalWrite(relayPin, HIGH);
+  // Serial.println("Relay OFF");
+  // delay(5000);
+
+  int raw = analogRead(SENSOR_PIN);
+  Serial.printf("Raw: %d | Approx moisture: higher=dry\n", raw);
+  delay(1000);
 }
