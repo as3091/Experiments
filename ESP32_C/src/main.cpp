08a109070aaa756e750ae78a7b3d4f14364dd07a
as3091 <as3091@gmail.com>
2026-05-11 13:35:57 +0530

    OTA and logs on MQTT

--- ESP32_C/src/main.cpp
+++ ESP32_C/src/main.cpp
@@ -1,26 +1,40 @@
 #include <Arduino.h>
 #include <ArduinoOTA.h>
-
 #include "secrets.h"
 #include "wifi_helper.h"
-
+#include "MQTT_stuff.h"
+#include "get_the_time.h"
 
 void setup() {
-    Serial.begin(115200);
-    delay(100);
+  Serial.begin(115200);
+  delay(1000);
 
-    // pinMode(SOLENOID_PIN, OUTPUT);
-    // digitalWrite(SOLENOID_PIN, HIGH);  // solenoid OFF on boot
+  // pinMode(SOLENOID_PIN, OUTPUT);
+  // digitalWrite(SOLENOID_PIN, HIGH);  // solenoid OFF on boot
 
-    wifi_setup(WIFI_SSID, WIFI_PASS);
+  if (wifi_setup(WIFI_SSID, WIFI_PASS))
+  {
+    Serial.println("\nWiFi connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 
-  // Set a hostname so you don't have to hunt for the IP address
-  ArduinoOTA.setHostname(ArduinoOTA_Hostname); 
-  ArduinoOTA.setPassword(ArduinoOTA_Password);
+    // Set a hostname so you don't have to hunt for the IP address
+    // ArduinoOTA.setHostname(ArduinoOTA_Hostname);
 
-  ArduinoOTA.begin();
+
+    // Set a hostname so you don't have to hunt for the IP address
+    ArduinoOTA.setHostname(ArduinoOTA_Hostname);  
+    ArduinoOTA.setPassword(ArduinoOTA_Password);
+
+    ArduinoOTA.begin();
+    mqttConnect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASS, MQTT_HOST, MQTT_PORT,TOPIC_DATA);
+    local_time_setup(GMT_OFFSET_SEC, DAYLIGHT_OFFSET_SEC, NTP_SERVER);
+  }
 }
 
 void loop() {
-  ArduinoOTA.handle(); // This is critical!
+  delay(5000);
+  ArduinoOTA.handle(); // This is critical!  
+  Serial.println(OTA_version);
+  publish_event(TOPIC_DATA, getFormattedTime().c_str());
 }
